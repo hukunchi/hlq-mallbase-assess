c16352d1b17d06a847bd1b7cb1a016c9b851935c
hukunchi <hu1932@163.com>
2017-11-14 15:36:43 +0800

    init

--- hlq-mallbase.access/src/main/java/com/hlq/mallbase/access/controller/IndexController.java
+++ hlq-mallbase.access/src/main/java/com/hlq/mallbase/access/controller/IndexController.java
@@ -1,45 +1,39 @@
-
- /**
+/**
  * Project Name:hlq-mallbase.access
  * File Name:IndexController.java
  * Package Name:com.hlq.mallbase.access.controller
  * Date:2017年11月6日下午3:12:02
  * Copyright (c) 2017, .
  *
-*/
+ */
 
 package com.hlq.mallbase.access.controller;
 
 import javax.annotation.Resource;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.hlq.mallbase.management.facade.dto.OrderDto;
-import com.hlq.mallbase.management.facade.intf.DemoFacade;
+import com.hlq.mallbase.management.facade.intf.UserInfoFacade;
 
 /**
- * @author   hukc
- * @version  
- * @since    JDK 1.7
- * @see 	 
+ * @author hukc
+ * @version
+ * @since JDK 1.7
+ * @see
  */
-@RestController
+@Controller
 @RequestMapping("")
 public class IndexController {
 	@Resource
-	private DemoFacade demoFacade;
-	
-	@ResponseBody
-	@RequestMapping(value = "/queryDemo", method = RequestMethod.GET)
-	public String queryDemo(String userName) throws Exception{
-		OrderDto orderDto=new OrderDto();
-		orderDto.setOrderId("sdfasf");
-		orderDto.setChannelId("sdfsdf");
-		demoFacade.insertOrder(orderDto);
-		return "success";
-	} 
-}
+	private UserInfoFacade userInfoFacade;
 
+	@RequestMapping("/")
+	public String index(Model model) {
+		int res = userInfoFacade.doLogin("123", "123");
+		System.out.println(res);
+		model.addAttribute("name", "dfsdfsf");
+		return "index";
+	}
+}
